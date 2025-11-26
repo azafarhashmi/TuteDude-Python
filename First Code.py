b59bd94ffe498a085eeaaa2622e4b8af2226ff69
azafarhashmi <azafarhashmi@gmail.com>
2025-11-27 03:44:38 +0530

    update

---
+++ First Code.py
@@ -0,0 +1,15 @@
+#task1
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+
+
+
+print("\n\n\n")
+#task2
+fn=input("Enter your first name: ")
+ln=input("Enter your last name: ")
+print(f"Hello,{fn} {ln}!, Welcome to the Pyhton Program")
