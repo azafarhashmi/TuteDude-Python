5a3f5679dd97bddcd1b23a93318549fc8e8730d4
Arham Zafar Hashmi <159815196+azafarhashmi@users.noreply.github.com>
2025-11-27 03:59:05 +0530

    Update First Code.py

--- First Code.py
+++ First Code.py
@@ -12,4 +12,4 @@ print("\n\n\n")
 #task2
 fn=input("Enter your first name: ")
 ln=input("Enter your last name: ")
-print(f"Hello,{fn} {ln}!, Welcome to the Pyhton Program")
+print(f"Hello,{fn} {ln}!, Welcome to the Python Program")
